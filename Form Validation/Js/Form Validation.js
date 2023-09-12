a7be9c7bf6baabf41d885330c44e8ab13bc27210
ChristianBernard05 <12345playchristianbernard@gmail.com>
2023-09-12 23:59:19 +0700

    first commit

---
+++ Form Validation/Js/Form Validation.js
@@ -0,0 +1,4 @@
+function btnClick(e) {
+    console.log(e)
+    document.getElementById("display"),value = e
+}
---
+++ JavaScript D-1.js
@@ -0,0 +1,20 @@
+var message1 = "Hello One"
+let message2 = "Hello Two"
+const message3 = "Hello Three"
+
+var name = "Bernard"
+var age = "18"
+var single = "true"
+
+typeof name;
+typeof age;
+
+
+var a = 5;
+var b = 3;
+var c = a + b;
+console.log(c);
+
+var variabelyangpertama;
+console.log(variabelyangpertama);
+
